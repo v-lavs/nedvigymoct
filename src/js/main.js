dc8b479685274ea606aa6d251066fd82b40bb9ee
v-lavs <lavskavanda@gmail.com>
2021-05-27 23:05:03 +0300

    add layout

---
+++ src/js/main.js
@@ -0,0 +1,50 @@
+/*
+* to include js file write: `//= include ./path-to-file`
+* */
+
+//= include ../../node_modules/jquery/dist/jquery.js ;
+
+//= include ../lib/waypoints/index.js
+
+// CUSTOM SCRIPTS
+
+$(document).ready(function () {
+
+    //MOBILE MENU
+    var nav = $('.header__nav');
+
+    $('.btn-burger').click(function (e) {
+        e.preventDefault();
+        nav.addClass('open');
+        jQuery('.backdrop').fadeIn();
+    });
+
+    $('.btn-close, .backdrop').click(function (e) {
+        e.preventDefault();
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
+
+
+    //HEADER SCROLL
+
+    function onHeaderScrol() {
+        scrolled = window.pageYOffset || document.documentElement.scrollTop;
+        if (scrolled > 40) {
+            jQuery(".header").addClass('header_active');
+        } else {
+            jQuery(".header").removeClass('header_active');
+        }
+    }
+
+    $(document).on('scroll', function () {
+        onHeaderScrol()
+    });
+
+    var bannerSlider = new Swiper(".banner-slider", {
+        pagination: {
+            el: ".swiper-pagination",
+        },
+    });
+
+});
