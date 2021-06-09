7150359648af4df4fdf2ad1da7af19463329e7e4
v-lavs <lavskavanda@gmail.com>
2021-06-09 13:22:42 +0300

    add changes

--- src/js/main.js
+++ src/js/main.js
@@ -2,8 +2,8 @@
 * to include js file write: `//= include ./path-to-file`
 * */
 
-//= include ../../node_modules/jquery/dist/jquery.js ;
-
+//= include ../../node_modules/jquery/dist/jquery.js
+//= include ../../node_modules/lottie-web/build/player/lottie.js
 //= include ../lib/lightbox2/js/lightbox.js
 //= include ../lib/waypoints/index.js
 
@@ -16,6 +16,26 @@ $(document).ready(function () {
     lightbox.option({
         disableScrolling: true,
         positionFromTop: 0
+    });
+
+    function hideHeader() {
+        $('.header').addClass('header_active');
+    }
+
+    // HEADER SCROLL
+
+    var header = $('.header'),
+        scrollPrev = 0;
+
+    $(window).scroll(function () {
+        var scrolled = $(window).scrollTop();
+
+        if (scrolled > 100 && scrolled > scrollPrev) {
+            header.addClass('header_active');
+        } else {
+            header.removeClass('header_active');
+        }
+        scrollPrev = scrolled;
     });
 
     function slidersInit() {
@@ -119,6 +139,8 @@ $(document).ready(function () {
         onHeaderScroll()
     });
 
+
+    //DOCUMENT LIST CHANGE
     $('#documents-list input').on('change', function () {
         const id = $(this).val();
         $('.section-documents .documents-list').fadeOut(700);
@@ -128,10 +150,27 @@ $(document).ready(function () {
         }, 701);
     });
 
+
+    //SLIDERS
+
     let bannerSlider = new Swiper(".banner", {
         grabCursor: true,
         pagination: {
             el: "#mainBannerPagination",
+            clickable: true
+        },
+    });
+
+    let singleBannerSlider = new Swiper(".single-banner-slider", {
+        grabCursor: true,
+        autoplay: {
+            delay: 4500,
+            disableOnInteraction: false,
+        },
+        spaceBetween: 10,
+        loop: true,
+        pagination: {
+            el: "#singleBannerSlide",
             clickable: true
         },
     });
@@ -159,8 +198,21 @@ $(document).ready(function () {
     });
 
 
-    //READ MORE BTN
+    // POPUP
+    $('.popup-trigger-consultation').click(function (e) {
+        e.preventDefault();
+        $('#popupConsultation').addClass('open_modal');
+        $('.backdrop').fadeIn();
+        $('body').addClass('open_modal');
+    })
 
+    $('#closePopup,  .backdrop').click(function () {
+        $('#popupConsultation').removeClass('open_modal');
+        $('.backdrop').fadeOut();
+        $('body').removeClass('open_modal');
+    });
+
+    //READ MORE BTN
     $('.btn_read-more').click(function(e) {
         e.preventDefault();
         $('.text-hide .mob-hide').removeClass('mob-hide');
@@ -170,12 +222,51 @@ $(document).ready(function () {
     //ANIMATION
     setTimeout(function () {
         $('.section-intro').addClass('banner_anim');
+        $('.section-banner').addClass('banner_anim');
     }, 400);
 
     var waypoints = $('.section_anim').waypoint(function (direction) {
         $(this.element).addClass('active')
     }, {
         offset: '80%'
+    });
+
+    // window.addEventListener('resize', function() {
+    //     window.require("lottie").lottie.resize();
+    // });
+
+
+    let animationDiv = document.getElementById('scrollingArea')
+    animationDiv.style.display = "none";
+    function map(div, pathLocation) {
+    let animationMap = bodymovin.loadAnimation({
+        container: document.getElementById('img_map'),
+        renderer: 'svg',
+        loop: false,
+        autoplay: true,
+        rendererSettings : {
+            preserveAspectRatio : 'xMidYMax slice'
+        },
+        path: 'Map_animation.json'
+    });
+    animationMap.play();
+}
+
+    window.addEventListener('scroll', () => {
+
+        let scrollHeightPercent = document.documentElement.scrollHeight * 0.08
+        let currentPOS = document.documentElement.scrollTop || document.body.scrollTop
+
+        if (currentPOS >= scrollHeightPercent) {
+            let animationDiv = document.getElementById('scrollingArea');
+            if (animationDiv.style.display === 'none') {
+
+                animationDiv.style.display= ""
+
+                let bodyMotion1 = document.getElementById('lottie-scroll-1');
+                map(bodyMotion1, 'Map_animation.json')
+            };
+        };
     });
 
 });
