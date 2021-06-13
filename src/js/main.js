12a8046281ae3dc10caae84de8291aa98f8a83ca
v-lavs <lavskavanda@gmail.com>
2021-06-13 10:46:30 +0300

    fix overlay popup

--- src/js/main.js
+++ src/js/main.js
@@ -287,7 +287,7 @@ $(document).ready(function () {
         var items = $('.search-table__tbody .search-table__row');
         setTimeout(function () {
             for (var i = 0; i < items.length; i++) {
-                $(items[i]).delay(i * 400).animate({opacity: 1}, 400);
+                $(items[i]).delay((items.length - i + 1) * 400).animate({opacity: 1}, 400);
             }
         }, 400);
     });
